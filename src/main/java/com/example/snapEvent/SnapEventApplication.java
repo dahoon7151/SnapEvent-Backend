6c77b7f8551a89134cbd2799981ef4558fb6b72f
dahoon <dahoon7151@gmail.com>
2024-03-17 17:22:57 +0900

    basic spring project with dependecy(mysql, jpa, spring web, spring security, lombok

---
+++ src/main/java/com/example/snapEvent/SnapEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.snapEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapEventApplication.class, args);
+	}
+
+}
