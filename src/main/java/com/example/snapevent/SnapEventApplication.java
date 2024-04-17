0c5382c3c6faee6a88220b63a690df129ae9e1da
dahoon kang <dahoon7151@gmail.com>
2024-04-17 20:34:10 +0900

    프로젝트명 변경 전 복구용

--- src/main/java/com/example/snapevent/SnapEventApplication.java
+++ src/main/java/com/example/snapevent/SnapEventApplication.java
@@ -1,4 +1,4 @@
-package com.example.snapevent;
+package com.example.snapEvent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
