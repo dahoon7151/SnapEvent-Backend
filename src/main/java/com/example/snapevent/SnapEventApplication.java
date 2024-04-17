e07a1764372b6267630c85ef16d41f1eafba7d46
dahoon kang <dahoon7151@gmail.com>
2024-04-17 20:40:45 +0900

    프로젝트명 변경 snapevent

--- src/main/java/com/example/snapevent/SnapEventApplication.java
+++ src/main/java/com/example/snapevent/SnapEventApplication.java
@@ -1,4 +1,4 @@
-package com.example.snapEvent;
+package com.example.snapevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
