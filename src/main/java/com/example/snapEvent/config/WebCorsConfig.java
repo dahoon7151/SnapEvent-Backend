a86e4388f55d644ad0974355b5c391abe7884774
seung-je <csz0209@gmail.com>
2024-05-07 14:03:39 +0900

    chore:CORS 설정(GET 요청만)

---
+++ src/main/java/com/example/snapEvent/config/WebCorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.snapEvent.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:5173")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization");
+    }
+}
