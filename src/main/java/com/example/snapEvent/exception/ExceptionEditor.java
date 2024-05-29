d028c55ba404ff0922b3a5b003953ae29335baaa
dahoon kang <dahoon7151@gmail.com>
2024-05-29 20:48:41 +0900

    valid 에러코드 200->500

--- src/main/java/com/example/snapEvent/exception/ExceptionEditor.java
+++ src/main/java/com/example/snapEvent/exception/ExceptionEditor.java
@@ -23,7 +23,7 @@ public class ExceptionEditor {
     ) {
         BindingResult bindingResult = e.getBindingResult();
         return ResponseEntity
-                .status(HttpStatus.OK)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(MethodInvalidResponse.builder()
                         .error(bindingResult.getFieldErrors().get(0).getCode())
                         .message(bindingResult.getFieldErrors().get(0).getDefaultMessage())
